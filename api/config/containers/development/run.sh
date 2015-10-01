62feee13ba2c462a97ec390850c7cdbe5bc4c57d
Sam Vevang <sam.vevang@gmail.com>
2015-10-01 10:20:27 -0500

    break out the shared folder run script

--- api/config/containers/development/run.sh
+++ api/config/containers/development/run.sh
@@ -4,19 +4,8 @@ set -x
 BIN_DIR=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 APP_ROOT=$BIN_DIR/../../../
 
-cat << EOF
- note that on boot2docker i had to create a shared folder as follows:
-
-   $ boot2docker halt
-   $ vboxmanage sharedfolder add "boot2docker-vm" --name totem-api --hostpath /Users/sam/src/totem/api/
-   $ boot2docker up
-   $ boot2docker ssh "sudo modprobe vboxsf && sudo mkdir /totem-api && sudo mount -v -t vboxsf  -o uid=9999,gid=9999 totem-api /totem-api"
-
-EOF
-
 docker run --name totem-api \
   -p 80:80 \
-  -v /totem-api:/home/app/totem-api \
   --link totem-db:totem-db \
   -e PASSENGER_APP_ENV=development \
   -d svevang/totem-api
