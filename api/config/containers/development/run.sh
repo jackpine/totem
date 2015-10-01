6e3f69f930de3c53ab4bbe5e0a2f43de480d296c
Sam Vevang <sam.vevang@gmail.com>
2015-10-01 09:43:24 -0500

    warn on boot2docker

--- api/config/containers/development/run.sh
+++ api/config/containers/development/run.sh
@@ -4,14 +4,15 @@ set -x
 BIN_DIR=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 APP_ROOT=$BIN_DIR/../../../
 
-# note that on boot2docker I had to create a shared folder as follows:
-#
-#   $ boot2docker halt
-#   $ vboxmanage sharedfolder add "boot2docker-vm" --name totem-api --hostpath /Users/ross/src/totem-api/
-#   $ boot2docker up
-#   $ boot2docker ssh "sudo modprobe vboxsf && sudo mkdir /totem-api && sudo mount -v -t vboxsf  -o uid=9999,gid=9999 totem-api /totem-api"
+cat << EOF
+ note that on boot2docker i had to create a shared folder as follows:
 
+   $ boot2docker halt
+   $ vboxmanage sharedfolder add "boot2docker-vm" --name totem-api --hostpath /Users/sam/src/totem/api/
+   $ boot2docker up
+   $ boot2docker ssh "sudo modprobe vboxsf && sudo mkdir /totem-api && sudo mount -v -t vboxsf  -o uid=9999,gid=9999 totem-api /totem-api"
 
+EOF
 
 docker run --name totem-api \
   -p 80:80 \
