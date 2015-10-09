2efdf0e373f2c1ff931c9bc082310a4a2ccc15e9
Sam Vevang <sam.vevang@gmail.com>
2015-10-09 13:33:06 -0500

    stub out the place model

--- api/app/models/place.rb
+++ api/app/models/place.rb
@@ -1,2 +1,14 @@
 class Place < ActiveRecord::Base
+  validates :name, presence: true, on: :create
+  validates :authoritative, inclusion: {in:[true, false]} , on: :create
+  validates :authoritative_boundary, presence: true, if: :authoritative?
+  validates :import_metadata, presence: true, if: :imported?
+
+  def authoritative?
+    !!authoritative
+  end
+
+  def imported?
+    !import_source.blank?
+  end
 end
--- api/spec/models/place_spec.rb
+++ api/spec/models/place_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe Place, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Place, type: :model do
+
+ let(:place) { Place.new(name: "The Empire State Building", authoritative: false) }
+
+ describe "#valid?" do
+   subject { place.valid? }
+
+   context "validating a regular place" do
+     it { should be true }
+   end
+
+   context "validating an 'authoritative' place" do
+     before {place.authoritative = true }
+     context "missing the bounding polygon" do
+       it { should be false }
+     end
+     context "with bounding polygon" do
+       before do
+         place.authoritative_boundary = "POLYGON ((35 10, 45 45, 15 40, 10 20, 35 10), (20 30, 35 35, 30 20, 20 30))"
+       end
+       it { should be true }
+     end
+   end
+
+   context "imported data" do
+     before { place.import_source = "open-geo-places.biz" }
+     context "importated place missing metadata" do
+       it { should be false }
+     end
+     context "importated place with metadata" do
+       before { place.import_metadata = {'placename_key': 'yahoo WOE'}}
+       it { should be true }
+     end
+   end
+ end
 end
