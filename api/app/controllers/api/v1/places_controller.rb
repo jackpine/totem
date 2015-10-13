69a3dd5628d855f5aacf69ed3b272343c1a6a74b
Sam Vevang <sam.vevang@gmail.com>
2015-10-13 14:22:45 -0500

    trim 200ms off the default query

--- api/app/controllers/api/v1/places_controller.rb
+++ api/app/controllers/api/v1/places_controller.rb
@@ -4,9 +4,13 @@ class Api::V1::PlacesController < Api::V1::BaseController
 
     location = sanitize_location_params(location_params)
 
-    distance_func = "ST_Distance(authoritative_boundary, ST_GeomFromText('POINT(#{location[0]} #{location[1]})', 4326))"
+    my_loc = "ST_GeomFromText('POINT(#{location[0]} #{location[1]})', 4326)"
+    distance_func = "ST_Distance(authoritative_boundary, #{my_loc}, true)"
+    within_func = "ST_DWithin(#{my_loc}, authoritative_boundary, 0.125)"
+
     @places = Place
       .select('id', 'name', "#{distance_func} as distance")
+      .where(within_func)
       .order("distance ASC, category DESC").limit(20);
 
     respond_to do |format|
