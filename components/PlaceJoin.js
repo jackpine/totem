53bed0c76a1d04a3271e12a5e7061edd1ba1beda
Sam Vevang <sam.vevang@gmail.com>
2015-10-20 21:43:28 -0500

    customize the nav bar with a plus icon

--- components/PlaceJoin.js
+++ components/PlaceJoin.js
@@ -3,6 +3,8 @@
 var React = require('react-native');
 var DebugLocation = require('./DebugLocation');
 var PlaceList = require('./PlaceList');
+var NavigationBar = require('./NavigationBar');
+var { Icon, } = require('react-native-icons');
 
 var {
     StyleSheet,
@@ -10,24 +12,67 @@ var {
     TextInput,
     View,
     ListView,
+    TouchableHighlight
 } = React;
 
 
-var PlaceCreate = React.createClass({
+var PlaceJoin = React.createClass({
+    statics: {
+        navBar: function(){
+            var leftButton = function(){};
+            var rightButton = function(){
+                return (<TouchableHighlight style={styles.placeCreateWrapper} onPress={() => console.log('foo') }>
+                    <Icon
+                        name='fontawesome|plus'
+                        size={20}
+                        color='black'
+                        style={styles.placeCreateButton} />
+                </TouchableHighlight>)
+            };
+            return (
+                <NavigationBar 
+                    leftButton={ leftButton }
+                    rightButton={ rightButton }
+                    />
+            );
+        }
+    },
     render: function() {
 
         var locationDebugInfo = <DebugLocation location={this.props.location}/>
 
         return (
-          <View>
-            <Text>Join one of the following places</Text>
+          <View style={styles.listWrapper}>
+            {locationDebugInfo}
             <PlaceList
               nearbyPlaces= { this.props.nearbyPlaces }
             />
           </View>
         );
     },
+    onPressCreatePlace: function(){
+      console.log('create a place!')
+    }
 });
+var styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: "row",
+  },
+  joinInstructions: {
 
-module.exports = PlaceCreate;
+  },
+  placeCreateWrapper: {
+    height: 28,
+    width: 28
+  },
+  placeCreateButton: {
+    height: 28,
+    width: 28
+  },
+  listWrapper: {
+    flex: 1
+  }
+}
+                              )
 
+module.exports = PlaceJoin;
