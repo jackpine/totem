ac5e706e674b3f40e5f1639abc2bbcd1a21f3493
Sam Vevang <sam.vevang@gmail.com>
2015-11-05 18:24:27 -0600

    safely print out debugging info

--- components/DebugLocation.js
+++ components/DebugLocation.js
@@ -11,12 +11,15 @@ module.exports = React.createClass({
 
     render: function() {
         var loc;
-        if(this.props.location)
-            loc = this.props.location[0];
+        if(this.props.location){
+            loc = Object.assign({}, this.props.location[0])
+            loc.lat = this.props.location[0].lat.toPrecision(9);
+            loc.lon = this.props.location[0].lon.toPrecision(9);
+        }
         else
             loc = {lat: 'missing', lon: 'missing'}
 
-        return (<Text style={styles.debugInfo}>{`Lat: ${loc.lat.toPrecision(9)} Lng: ${loc.lon.toPrecision(10)} HrzAccurc: ${loc.horizontalAccuracy}`}</Text>)
+        return (<Text style={styles.debugInfo}>{`Lat: ${loc.lat} Lng: ${loc.lon} HrzAccurc: ${loc.horizontalAccuracy}`}</Text>)
     }
 
 })
