1f6274273048a2108e560c722edbeaeca3193ddd
Sam Vevang <sam.vevang@gmail.com>
2016-06-14 12:31:27 -0700

    ensure that we are always creating a non-point boundary

---
+++ api/db/migrate/20160614192525_update_place_calculate_boundary.rb
@@ -0,0 +1,11 @@
+class UpdatePlaceCalculateBoundary < ActiveRecord::Migration
+  def up
+    path = Rails.root.join('db/sql/places.sql')
+    raw_sql = File.read(path)
+    ActiveRecord::Base.connection.execute(raw_sql)
+  end
+
+  def down
+    ActiveRecord::Base.connection.execute("drop function place_calculate_boundary(geometry, geometry);")
+  end
+end
