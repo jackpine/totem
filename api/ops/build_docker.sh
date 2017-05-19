713bfb9aec1da1d1a13a83e61067e931be2b4d35
Sam Vevang <sam.vevang@gmail.com>
2017-05-19 13:33:34 -0500

    fixup paths for docker helper build scripts

--- api/ops/build_docker.sh
+++ api/ops/build_docker.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e
+set -x
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . $DIR/docker_machine.sh
 ensure_docker_machine
 
-./$DIR/_build_docker_api.sh
-./$DIR/_build_docker_admin.sh
+$DIR/_build_docker_api.sh
+$DIR/_build_docker_admin.sh
